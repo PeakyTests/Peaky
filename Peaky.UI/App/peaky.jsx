79b5705eb13059d2dc61e87cc6edfda629f83295
Phillip Pruett <phpruett@microsoft.com>
2016-08-10 16:16:26 -0700

    test run history icons and initial runAll button

--- Peaky.UI/App/peaky.jsx
+++ Peaky.UI/App/peaky.jsx
@@ -2,6 +2,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var htmlContent = require('../App/peaky.html');
+var update = require('immutability-helper');
+require("babel-polyfill");
 
 var insertKnockoutBindingsIntoDom = function () {
     var div = document.createElement('div');
@@ -11,6 +13,7 @@ var insertKnockoutBindingsIntoDom = function () {
 }
 
 var testResults = [];
+var uniqueIds = 0;
 
 var Sandwich = React.createClass({
     render: function () {
@@ -18,12 +21,12 @@ var Sandwich = React.createClass({
          <div className="Sandwich">
             <h1>Peaky!</h1>
             <div className="testsAndResults">
-                <AvailableTests runTest={this.runTest } />
+                <AvailableTests runTest={this.runTest} testResults={this.state.testResults} />
                 <div className="results">
                     {
-                        this.state.testResults.map((test) =>
-                            <div className="result">
-                                <div className={test.result.toLowerCase() }>{test.result} - {test.target} - {test.name}</div>
+                        this.state.testResults.map((test, i) =>
+                            <div key={i} className="result">
+                                <h3 className={test.result.toLowerCase() }>{test.result} - {test.target} - {test.name}</h3>
                                 <pre><code className="json">{test.raw}</code></pre>
                             </div>
                         )
@@ -42,30 +45,38 @@ var Sandwich = React.createClass({
     runTest: function (test, sectionName) {
 
         var sandwich = this;
+
+        var key = uniqueIds++;
+        var newState = update(sandwich.state.testResults, {
+            $push: [{
+                result: 'Pending',
+                name: getTestName(test.Url),
+                url: test.Url,
+                target: sectionName,
+                key: key,
+                raw: "{}"
+            }]
+        });
+        sandwich.setState({ testResults: newState });
+
         $.ajax({
             url: test.Url,
             type: "POST",
             data: {},
             dataType: 'json',
             success: function (data) {
-                testResults.unshift({
-                    result: 'Passed',
-                    name: getTestName(test.Url),
-                    target: sectionName,
-                    raw: JSON.stringify(data.responseJSON || data.responseText || {}, null, 2).replace(/[\\]+r[\\]+n/g, "\n")
-                });
-                sandwich.setState({ testResults: testResults });
-                hljs.highlightBlock($('pre code').first()[0]);
+                var result = sandwich.state.testResults.find(t => t.key == key);
+                result.result = 'Passed';
+                result.raw = JSON.stringify(data.responseJSON || data.responseText || {}, null, 2).replace(/[\\]+r[\\]+n/g, "\n");
+                sandwich.setState({ testResults: sandwich.state.testResults });
+                //hljs.highlightBlock($('pre code').first()[0]);
             },
             error: function (data) {
-                testResults.unshift({
-                    result: 'Failed',
-                    name: getTestName(test.Url),
-                    target: sectionName,
-                    raw: JSON.stringify(data.responseJSON || data.responseText || {}, null, 2).replace(/[\\]+r[\\]+n/g, "\n")
-                });
-                sandwich.setState({ testResults: testResults });
-                hljs.highlightBlock($('pre code').first()[0]);
+                var result = sandwich.state.testResults.find(t => t.key == key);
+                result.result = 'Failed';
+                result.raw = JSON.stringify(data.responseJSON || data.responseText || {}, null, 2).replace(/[\\]+r[\\]+n/g, "\n");
+                sandwich.setState({ testResults: sandwich.state.testResults });
+                //hljs.highlightBlock($('pre code').first()[0]);
             },
         });
     }
@@ -73,12 +84,16 @@ var Sandwich = React.createClass({
 
 var AvailableTests = React.createClass({
     loadTests: function () {
+        var url = "/" + (location.pathname + location.search).substr(1);
         $.ajax({
-            url: "/" + (location.pathname + location.search).substr(1),
+            url: url,
             context: document.body,
             dataType: 'json',
             success: function (data) {
-                this.setState({ data: groupTests(data.Tests) });
+                this.setState({
+                    data: groupTests(data.Tests),
+                    id: 1
+                });
             }.bind(this),
             error: function (xhr, status, err) {
                 console.error('#GET Error', status, err.toString());
@@ -86,8 +101,13 @@ var AvailableTests = React.createClass({
         });
     },
 
+    runAll: function(testGroup) {
+        testGroup.Tests.forEach(t => this.props.runTest(t, testGroup.sectionName))
+    },
+
     getInitialState: function () {
         return {
+            id: 1,
             data: []
         };
     },
@@ -99,23 +119,40 @@ var AvailableTests = React.createClass({
     render: function () {
         var currentTests = this;
         return (
-          <div className="tests">
-              {
-              this.state.data.map(function (testGroup) {
-                  return  <div>
+            <div className="AvailableTests" key={0}>
+                {
+              this.state.data.map(function (testGroup, i) {
+                  return <div key={i} data={i}>
                           <h2 className="sectionName">{testGroup.sectionName}</h2>
                       {
-                                  testGroup.Tests.map(function (test, i) {
-                                      return <div className="test" onClick={currentTests.props.runTest.bind(null, test, testGroup.sectionName)}>
-                                        <i className="fa fa-arrow-circle-right"></i>
-                                        <div className="testName">{test.TestName}</div>
+                                  testGroup.Tests.map(function (test, j) {
+                                      return <div key={j} className="testandhistory" data={j}>
+                                        <div className="test" onClick={currentTests.props.runTest.bind(null, test, testGroup.sectionName)}>
+                                            <i className="fa fa-arrow-circle-right"></i>
+                                            <div className="testName">{test.TestName}</div>
+                                        </div>
+                                        <div className="history">
+                                            {
+                                            currentTests.props.testResults.filter(i => i.url == test.Url).map((t,k) => {
+                                                var icon = "fa fa-spinner fa-pulse fa-fw";
+                                                if (t.result == "Passed") {
+                                                    icon = "fa fa-check-circle-o"
+                                                };
+                                                if (t.result == "Failed") {
+                                                    icon = "fa fa-times-circle-o"
+                                                };
+                                                return <i className={icon + ' ' + t.result.toLowerCase()} key={k} data={k} aria-hidden="true"></i>
+                                            }
+                                            )}
+                                        </div>
                                       </div>
                                   })
-                      }
+                    }
+                    <div onClick={currentTests.runAll.bind(currentTests, testGroup)}>Run All</div>
                   </div>
               })
-              }
-          </div>);
+                }
+            </div>);
     },
 });
 
@@ -134,7 +171,8 @@ var groupTests = function (tests) {
     var mappedTests = groupedTests.map(entry => ({
         sectionName: (entry.key.Application + ' ' + entry.key.Environment).toUpperCase(),
         key: entry.key,
-        Tests: entry.vals.map(test => ({
+        Tests: entry.vals.map((test, i) => ({
+            key: i,
             TestName: getTestName(test.Url),
             Url: test.Url,
             Tags: test.Tags
