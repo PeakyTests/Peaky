16add55ee175ad868e545a9392c8529cbb124e39
Phillip Pruett <phpruett@microsoft.com>
2016-09-26 15:42:30 -0700

    change sample page links to github pages source

--- Peaky/HttpConfigurationExtensions.cs
+++ Peaky/HttpConfigurationExtensions.cs
@@ -84,7 +84,7 @@ namespace Peaky
             string baseUri = "tests",
             IEnumerable<Type> concreateTestClasses = null,
             HttpMessageHandler handler = null,
-            string testUiScriptUrl = null,
+            string testUiScriptUrl = "http://phillippruett.github.io/Peaky/javascripts/peaky.js",
             IEnumerable<string> testUiLibraryUrls = null,
             IEnumerable<string> styleSheetUrls = null)
         {
@@ -113,7 +113,7 @@ namespace Peaky
                 .Then(a => a.Where(u => !string.IsNullOrEmpty(u)
                                         && Uri.IsWellFormedUriString(u, UriKind.RelativeOrAbsolute))
                     .ToArray())
-                .Else(() => new string[] { });
+                .Else(() => new string[] { "http://phillippruett.github.io/Peaky/stylesheets/peaky.css" });
             configuration.StyleSheets(styleSheetUrls);
 
             var testUiLibraryUrlsArray = testUiLibraryUrls
@@ -121,7 +121,7 @@ namespace Peaky
                 .Then(a => a.Where(u => !string.IsNullOrEmpty(u)
                                         && Uri.IsWellFormedUriString(u, UriKind.RelativeOrAbsolute))
                     .ToArray())
-                .Else(() => new string[] {});
+                .Else(() => new string[] { "http://phillippruett.github.io/Peaky/javascripts/vendors.js" });
             configuration.TestLibraryUrisAre(testUiLibraryUrlsArray);
 
             var testRootRouteTemplate = baseUri.AppendSegment("{environment}/{application}");
