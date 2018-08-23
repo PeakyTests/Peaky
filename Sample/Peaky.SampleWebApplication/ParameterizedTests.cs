0a1fb93f2f3a7722c0df2256a7ceb14d4f4e96da
Diego <dicolomb@microsoft.com>
2018-08-23 18:55:20 +0100

    /testcase_override_defaults: Auto stash before rebase of "master"

--- Sample/Peaky.SampleWebApplication/ParameterizedTests.cs
+++ Sample/Peaky.SampleWebApplication/ParameterizedTests.cs
@@ -34,12 +34,26 @@ namespace Peaky.SampleWebApplication
 
         }
 
+        public void parameterised_test_with_enum(TestParameterEnum value)
+        {
+
+        }
+
         public void RegisterTestCasesTo(TestDependencyRegistry registry)
         {
             registry.RegisterParametersFor(() => parameterised_test(1));
             registry.RegisterParametersFor(() => parameterised_test(2));
             registry.RegisterParametersFor(() => parameterised_test(3));
             registry.RegisterParametersFor(() => parameterised_test(4));
+
+            registry.RegisterParametersFor(() => parameterised_test_with_enum(TestParameterEnum.First));
+            registry.RegisterParametersFor(() => parameterised_test_with_enum(TestParameterEnum.Second));
         }
+    }
+
+    public enum TestParameterEnum
+    {
+        First,
+        Second
     }
 }
